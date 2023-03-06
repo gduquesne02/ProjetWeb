c56f8b0437898ae8a8889909a0332c0d0d303520
AlexandreALT <alex.ant2510@gmail.com>
2023-03-06 13:28:04 +0100

    connexion

---
+++ firstConnection.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Alpage Web - Connexion</title>
+  <link rel="stylesheet" href="./css/index.css" />
+  <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+</head>
+
+<body>
+  <form method="post" class="form">
+    <h2>Changez votre mot de passe</h2>
+    <div class="form-label">
+      <label for="password">Mot de passe</label>
+    </div>
+    <input type="password" name="password" class="form-input" /><br /><br />
+    <div class="form-label">
+      <label for="confirmPassword">Confirmation</label>
+    </div>
+    <input type="password" name="confirmPassword" class="form-input" /><br /><br />
+    <button class="form-submit">Changez le mot de passe</button>
+  </form>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <title>Alpage Web - Connexion</title>
+  <link rel="stylesheet" href="./css/index.css" />
+  <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+</head>
+
+<body>
+  <form method="post" class="form">
+    <h2>Connexion</h2>
+    <div class="form-label"><label for="mail">E-mail</label></div>
+    <input type="text" name="mail" class="form-input" /><br /><br />
+    <div class="form-label">
+      <label for="password">Mot de passe</label>
+    </div>
+    <input type="password" name="password" class="form-input" /><br /><br />
+    <a href="#" class="form-forgetPassword">Mot de passe oublié ?</a><br /><br />
+    <input type="submit" value="Connexion" class="form-submit" />
+  </form>
+</body>
+
+</html>
