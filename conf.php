6609ee02b7061b794b4154dff6a1e8b804fa7c9e
AlexandreALT <alex.ant2510@gmail.com>
2023-03-13 13:49:17 +0100

    connexion

---
+++ conf.php
@@ -0,0 +1,3 @@
+<?php
+
+$bdd = new PDO('mysql:host=mysql-alpageweb.alwaysdata.net;dbname=alpageweb_iut', 'alpageweb', 'Z@X4w3SgSmgEhLQ');
--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+require("./conf.php");
+
+if (isset($_POST['email']) && isset($_POST['password'])) {
+  $req = "SELECT * FROM users WHERE email = :email AND password = :password";
+  $res = $bdd->prepare($req);
+  $res->bindParam(":email", $_POST['email']);
+  $hashPassword = hash('sha256', $_POST['password']);
+  $res->bindParam(":password", $hashPassword);
+  $res->execute();
+  $data = $res->fetchAll(PDO::FETCH_ASSOC);
+  if (count($data) === 0) echo "<div class='wrongIdentification'>Identifiants incorrects</div>";
+  else {
+    session_start();
+    $_SESSION['idUser'] = $data[0]['id'];
+    $_SESSION['email'] = $data[0]['email'];
+    $_SESSION['firstName'] = $data[0]['firstname'];
+    $_SESSION['lastName'] = $data[0]['lastname'];
+    $_SESSION['iut'] = $data[0]['iut'];
+    echo "Connexion réussie";
+  }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,8 +37,8 @@
 <body>
   <form method="post" class="form">
     <h2>Connexion</h2>
-    <div class="form-label"><label for="mail">E-mail</label></div>
-    <input type="text" name="mail" class="form-input" /><br /><br />
+    <div class="form-label"><label for="email">E-mail</label></div>
+    <input type="text" name="email" class="form-input" /><br /><br />
     <div class="form-label">
       <label for="password">Mot de passe</label>
     </div>
